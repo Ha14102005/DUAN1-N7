0e3406467c90ee6cd3734878184fed96c68b476b
Ha14102005 <hoangha14102005@gmail.com>
2024-11-06 21:36:30 +0700

    ok giaodien

--- DA1-N7/index.php
+++ DA1-N7/index.php
@@ -35,6 +35,17 @@
                 <input type="text" name="query" required placeholder="Nhập máy tính, phụ kiện... cần tìm">
                 <button type="submit">Tìm kiếm</button>
             </form>
+            <?php
+
+            if (isset($_GET['query'])) {
+                $query = $_GET['query'];
+                $sql = "SELECT * FROM product WHERE name LIKE '%$query%'";
+                $products = $conn->query($sql);
+            } else {
+                $sql = "SELECT * FROM product";
+                $products = $conn->query($sql);
+            }
+            ?>
             <div class="account-button">
                 <img src="slide/icon.png" alt="Account" width="20px">
                 <span><a href="">Giỏ hàng</a></span>
@@ -62,7 +73,7 @@
                     <?php
                     if ($products->num_rows > 0) {
                         while ($product = $products->fetch_assoc()) {
-                            
+
                     ?>
                             <div class="product-item">
                                 <img src="<?php echo $product['image_src']; ?>" alt="<?php echo $product['name']; ?>" width="150px">
@@ -137,6 +148,55 @@
         </div>
 
         <div class="footer">
+        <footer>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h3>Thông tin công ty</h3>
+                <p>LaptopStore.vn</p>
+                <p>CÔNG TY CỔ PHẦN  VIỆT NAM</p>
+                <p> Quận Long Biên, Thành phố Hà Nội, Việt Nam</p>
+                <p>SĐT: 024.9999.9999</p>
+                <p>Website: laptop88.vn</p>
+                <p>Sở KHĐT TP. Hà Nội cấp</p>
+            </div>
+            <div class="col">
+                <h3>Về LaptopStore.vn</h3>
+                <ul>
+                    <li>Giới thiệu chung</li>
+                    <li>Tuyển dụng</li>
+                    <li>Liên hệ</li>
+                </ul>
+            </div>
+            <div class="col">
+                <h3>Chính sách</h3>
+                <ul>
+                    <li>Chính sách mua hàng từ xa - kiểm hàng</li>
+                    <li>Chính sách đặt cọc sản phẩm</li>
+                    <li>Chính sách giao nhận - đổi trả</li>
+                    <li>Hướng dẫn thanh toán trực tuyến</li>
+                    <li>Chính sách bảo hành</li>
+                    <li>Chính sách bảo mật thông tin</li>
+                    <li>Quy trình tiếp nhận và giải quyết khiếu nại</li>
+                    <li>Thỏa thuận sử dụng và quy định giao dịch chung</li>
+                </ul>
+            </div>
+            <div class="col">
+                <h3>Thanh toán</h3>
+                <ul>
+                    <li>Thanh toán trực tuyến (Internet Banking)</li>
+                    <li>Thanh toán khi nhận hàng (COD)</li>
+                    <li>Hướng dẫn thanh toán VNPay</li>
+                </ul>
+                <img src="" alt="Phương thức thanh toán">
+            </div>
+        </div>
+    </div>
+    <div class="social">
+        <a href="#" class="fab fa-facebook-f"></a>
+        <a href="#" class="fab fa-google-plus-g"></a>
+    </div>
+</footer>
         </div>
     </div>
 
